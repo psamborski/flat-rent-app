de7a7c0b95303983ed0451714aa5a40d5db112e4
psamborski <psambek@gmail.com>
2025-02-27 00:41:33 +0100

    Installed tailwindcss and shadcn libraries.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import HomePage from './pages/HomePage'
+// import MapPage from './pages/MapPage'
+// import PropertyPage from './pages/PropertyPage'
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className='app'>
+        <span />
+        <Routes>
+          <Route
+            path='/'
+            element={<h1 className='text-2xl font-bold underline'> Hello world! </h1>}
+          />
+          {/*<Route path="/rent" element={<MapPage />} />*/}
+          {/*<Route path='/offer/:id' element={<PropertyPage />} />*/}
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
