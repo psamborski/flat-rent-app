bdf7145e9c51af3cb676e7a65204ec130b9da315
psamborski <psambek@gmail.com>
2025-03-01 01:03:19 +0100

    Added some dummy data and enpoints.

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,4 +1,14 @@
+interface DummyData {
+  cities: City[]
+  districts: Record<string, District[]>
+}
+
 interface City {
+  id: number
+  name: string
+}
+
+interface District {
   id: number
   name: string
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import data from '@/static/data.json'
 
+// give a data appropriate type
+const typedData = data as DummyData
+
 export const getAllCities = async (): Promise<City[]> => {
   // data latency simulation
   await new Promise((resolve) => setTimeout(resolve, 200))
 
-  // return cities
-  return data
+  return typedData.cities
 }
 
 export const getFilteredCities = async (filter: string): Promise<City[]> => {
@@ -14,10 +16,20 @@ export const getFilteredCities = async (filter: string): Promise<City[]> => {
 
   // cities filtering
   const lowerFilter = filter.toLocaleLowerCase()
-  return data.filter((city: City) => city.name.toLocaleLowerCase().startsWith(lowerFilter))
+  return typedData.cities.filter((city: City) =>
+    city.name.toLocaleLowerCase().startsWith(lowerFilter),
+  )
   // .slice(0, 20)
   // .map((city: City) => ({
   //   id: city.id,
   //   name: city.name,
   // }))
+}
+
+export const getDistrictsByCity = async (cityId: number): Promise<District[]> => {
+  // data latency simulation
+  await new Promise((resolve) => setTimeout(resolve, 200))
+
+  // return cities
+  return typedData.districts[cityId.toString()] ?? []
 }
